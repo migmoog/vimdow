da8092873c34e03d999a587d2623798394dc223d
migmoog <57271984+migmoog@users.noreply.github.com>
2026-06-24 18:30:00 -0400

    more advanced tasks

---
+++ build_targets.py
@@ -0,0 +1,88 @@
+import subprocess
+from enum import StrEnum
+import subprocess
+from dataclasses import dataclass
+from invoke import Context
+
+def cargo_cmd(method: str, *args) -> str:
+    cmd = f"cargo {method} {' '.join(args)} --manifest-path=rust/Cargo.toml"
+    return cmd
+
+class BuildProfile(StrEnum):
+    BOTH = "both"
+    DEBUG = "debug"
+    RELEASE = "release"
+
+    def get_list(self):
+        if self == BuildProfile.BOTH:
+            return ["debug", "release"]
+        else:
+            return [self.value]
+
+@dataclass
+class Platform:
+    rust_target: str
+    libfile: str
+
+    def target_flag(self) -> str:
+        return f" --target {self.rust_target} "
+
+    def build(self, c: Context, profile: BuildProfile) -> list[str]:
+        tf = self.target_flag()
+        profiles = {}
+        match profile:
+            case BuildProfile.DEBUG:
+                profiles[profile.value] = cargo_cmd("build", tf)
+            case BuildProfile.RELEASE:
+                profiles[profile.value] = cargo_cmd("build", tf, "--release")
+            case BuildProfile.BOTH:
+                profiles["debug"] = cargo_cmd("build", tf)
+                profiles["release"] = cargo_cmd("build", tf, "--release")
+
+        out = []
+        for key, value in profiles.items():
+            print(f"Building for {key} target {self.rust_target}")
+            c.run(value)
+            out.append(f"rust/target/{key}/{self.libfile}")
+        return out
+
+TARGETS = {
+    "Windows" : Platform( rust_target="x86_64-pc-windows-msvc", libfile="vimdow.dll" ),
+    "Darwin" : Platform( rust_target="x86_64-apple-darwin", libfile="libvimdow.dylib" ),
+    "Linux" : Platform( rust_target="x86_64-unknown-linux-gnu", libfile="libvimdow.so" ),
+}
+
+def available_targets() -> dict[str, str]:
+    result = subprocess.run(
+        ["rustup", "target", "list", "--installed"],
+        capture_output=True,
+        check=True,
+        text=True,
+    )
+    print(result.stdout)
+    return {k: v for k, v in TARGETS.items() if v.rust_target in result.stdout}
+
+
+def flag_to_platforms(flag: str) -> list[Platform]:
+    out = []
+    at = available_targets()
+    if "w" in flag:
+        if "Windows" in at:
+            out.append(at["Windows"])
+        else:
+            print(f"""Windows target is unavailable. Try 'rustup target add {TARGETS["Windows"].rust_target}'""")
+    if "m" in flag:
+        if "Darwin" in at:
+            out.append(at["Darwin"])
+        else:
+            print(f"""Mac target is unavailable. Try 'rustup target add {TARGETS["Darwin"].rust_target}'""")
+
+    if "l" in flag:
+        if "Linux" in at:
+            out.append(at["Linux"])
+        else:
+            print(f"""Linux target is unavailable. Try 'rustup target add {TARGETS["Linux"].rust_target}'""")
+
+    if len(out) == 0:
+        raise RuntimeError("No platform flag supplied")
+    return out
--- tasks.py
+++ tasks.py
@@ -1,40 +1,28 @@
+from decimal import Context
+from build_targets import flag_to_platforms
+from build_targets import cargo_cmd
 import commentedconfigparser
 import shutil
-from enum import StrEnum
+import dotenv
 from invoke.exceptions import PlatformError
 from invoke import task, call
 import platform
 import os
-from shutil import copy
+from shutil import copy, rmtree
+from build_targets import BuildProfile, TARGETS
 
-GDPATH = os.getenv("GDPATH")
-if not GDPATH:
-    GDPATH = "godot"
+dotenv.load_dotenv()
 
 def gd_cmd(*args) -> str:
-    return f"\"{GDPATH}\" {" ".join(args)} --path godot/"
-
-class BuildProfile(StrEnum):
-    BOTH = "both"
-    DEBUG = "debug"
-    RELEASE = "release"
+    if not os.environ["GDPATH"]:
+        raise RuntimeError("No GDPATH set. Cannot do Godot commands")
+    return f"\"{os.environ[ "GDPATH" ]}\" {" ".join(args)} --path godot/"
 
 SYSTEM = platform.system()
-LIB=None
-match SYSTEM:
-    case "Windows":
-        LIB = "vimdow.dll"
-    case "Linux":
-        LIB = "libvimdow.so"
-    case "Darwin":
-        LIB = "libvimdow.dylib"
-if LIB is None:
+current_platform = TARGETS[SYSTEM]
+if not current_platform:
     raise PlatformError("On an unsupported platform: " + SYSTEM)
 
-CARGO_TOML = "rust/Cargo.toml"
-def cargo_cmd(method: str, *args) -> str:
-    cmd = f"cargo {method} {' '.join(args)} --manifest-path={CARGO_TOML}"
-    return cmd
 
 def init_dir(path):
     if not os.path.exists(path):
@@ -44,73 +32,97 @@ def init_dir(path):
 def clean(c):
     print("Cleaning cargo build")
     c.run(cargo_cmd("clean"))
-    shutil.rmtree("godot/addons/vimdow/bin")
-    shutil.rmtree("build/")
+    rmtree("godot/addons/vimdow/bin")
+    rmtree("build/")
+    os.remove("godot/project.godot")
 
 @task(
     help = {
         "clean" : "Clean old build before compiling",
-        "profile" : "Type of profile to build. Should only be \"debug\", \"release\", or \"both\""
+        "profile" : "Type of profile to build. Should only be \"debug\", \"release\", or \"both\"",
+        "platform" : """Type of OS target to compile the rust library for. 
+All are in compound flag form.
+t- ex: 'l' for linux, 'w' for windows, 'm' for mac
+t- ex: 'lw' for linux and windows, 'lwm' for all 
+"""
     },
     aliases=["b"]
 )
-def build(c, profile: BuildProfile = None, clean=False):
+def build(c: Context, profile: BuildProfile = BuildProfile.DEBUG, platform: str = "", clean=False):
     if clean: 
         clean(c)
 
     init_dir("godot/addons/vimdow/bin")
     init_dir("build/")
+    if not os.path.exists("godot/project.godot"):
+        print("Making project.godot")
+        with open("godot/project.godot", "w") as f:
+            f.writelines([
+              'config_version=5',
+              '[application]',
+              'config/name="Vimdow"',
+              'config/features=PackedStringArray("4.6.2", "Forward Plus")',
+              'config/icon="res://icon.svg"',
+              'run/main_scene="res://addons/vimdow/vimdow_editor.tscn"',
+              '[display]',
+              'window/stretch/mode="canvas_items"',
+              'window/stretch/aspect="expand"',
+              '[editor_plugins]',
+              'enabled=PackedStringArray("res://addons/vimdow/plugin.cfg")',
+              '[physics]',
+              '3d/physics_engine="Jolt Physics"',
+              '[rendering]',
+              'rendering_device/driver.windows="d3d12"',
+              'textures/vram_compression/import_etc2_astc=true',
+            ])
+    libfiles = None
+    if platform:
+        platforms = flag_to_platforms(platform)
+        for p in platforms:
+            libfiles = p.build(c, profile)
+    else: 
+        libfiles = current_platform.build(c, profile)
 
-    profile = profile or BuildProfile.DEBUG
-    profiles: dict = {}
-    match profile:
-        case BuildProfile.DEBUG:
-            profiles[profile.value] = cargo_cmd("build")
-        case BuildProfile.RELEASE:
-            profiles[profile.value] = cargo_cmd("build", "--release")
-        case _:
-            profiles["debug"] = cargo_cmd("build")
-            profiles["release"] = cargo_cmd("build", "--release")
+    DST = "godot/addons/vimdow/bin"
+    for src in libfiles:
+        for dst_dir in profile.get_list():
+            destination = os.path.join(DST, dst_dir)
+            destination = os.path.join(destination, os.path.basename(src))
+            print(f"Copying {src} to {destination}")
+            os.makedirs(os.path.dirname(destination), exist_ok=True)
+            copy(src, destination)
 
-    for key, value in profiles.items():
-        print(f"Building for {key}")
-        c.run(value)
-        src = f"rust/target/{key}/{LIB}"
-        dst = "godot/addons/vimdow/bin/" + key
-        if not os.path.exists(dst):
-            print(f"Making profile location in godot/addons/vimdow/bin/{key}/")
-            os.mkdir(dst)
-        dst = os.path.join(dst, LIB)
-        copy(src, dst)
 
 @task(
     help={
         "clean" : "Clean old build before compiling",
-        "profile" : "The profile of the library to build. Default is \"debug\""
+        "profile" : "The profile of the library to build. Default is \"debug\"",
+        "platform" : "Same as in 'build' task"
     },
     aliases=["s"]
 )
-def standalone(c, nobuild=False, profile: BuildProfile = BuildProfile.DEBUG, clean=False):
+def standalone(c, nobuild=False, profile: BuildProfile = BuildProfile.DEBUG, platform: str = "", clean=False):
     if not nobuild:
-        build(c, profile, clean)
+        build(c, profile, platform, clean)
     c.run(gd_cmd())
 
 @task(
     help={
         "clean" : "Clean old build before compiling",
-        "profile" : "The profile of the library to build. Default is \"debug\""
+        "profile" : "The profile of the library to build. Default is \"debug\"",
+        "platform" : "Same as in 'build' task"
     },
     aliases=["e"],
     default=True
 )
-def editor(c, nobuild=False, profile: BuildProfile = BuildProfile.DEBUG, clean=False):
+def editor(c, nobuild=False, profile: BuildProfile = BuildProfile.DEBUG, platform: str = "", clean=False):
     if not nobuild:
-        build(c, profile, clean)
+        build(c, profile, platform, clean)
     c.run(gd_cmd("-e"))
 
 
 @task(
-    pre = [call(build, profile=BuildProfile.BOTH)],
+    pre = [call(build, profile=BuildProfile.BOTH, platform="lwm")],
     aliases = ["ep"]
 )
 def export_plugin(c):
@@ -128,11 +140,12 @@ def export_plugin(c):
 @task(
     aliases = ["es"],
     help = {
-     "profile" : "The profile of the library to build. Default is \"debug\""
+     "profile" : "The profile of the library to build. Default is \"debug\"",
+     "platform" : "Which platform to build for",
     }
 )
-def export_standalone(c, profile=BuildProfile.DEBUG):
-    build(c, profile)
+def export_standalone(c, profile=BuildProfile.DEBUG, platform: str = ""):
+    build(c, profile, platform)
     
     preset_name = "vimdow-standalone-"
     export_file = "vimdow."
